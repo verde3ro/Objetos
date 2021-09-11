d06ab451342e2e4a6a13af17b5d64e33541284af
verde3ro <verde3ro@gmail.com>
2021-09-11 12:39:19 -0500

    Commit inicial

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import com.appstracta.Carro;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Carro carro = new Carro(); // crear objeto o instancia
+        System.out.println("Hello, World!");
+    }
+}
