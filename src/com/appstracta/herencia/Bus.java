13e0ef295c8cf06f433bb0ad0bf8e32559eacea2
verde3ro <verde3ro@gmail.com>
2021-09-18 12:41:35 -0500

    Se agrega Clase Vehiculo y Coche 04

---
+++ src/com/appstracta/herencia/Bus.java
@@ -0,0 +1,15 @@
+package com.appstracta.herencia;
+
+public class Bus extends Vehiculo {
+
+	private int ejes;
+
+	public int getEjes() {
+		return this.ejes;
+	}
+
+	public void setEjes(int ejes) {
+		this.ejes = ejes;
+	}
+
+}
---
+++ src/com/appstracta/herencia/Moto.java
@@ -0,0 +1,11 @@
+package com.appstracta.herencia;
+
+public class Moto extends Vehiculo {
+
+	// Sobre escritura de metodos
+	@Override
+	public String doblar() {
+		return "mueve manubrio";
+	}
+
+}
