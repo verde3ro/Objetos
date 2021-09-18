4044f92d6a63580bc16b40ae4ebcbc19f02ff122
verde3ro <verde3ro@gmail.com>
2021-09-18 12:38:12 -0500

    Se agrega Clase Vehiculo y Coche 02

--- src/AppHerencia.java
+++ src/AppHerencia.java
@@ -1,4 +1,7 @@
+import com.appstracta.herencia.Bus;
 import com.appstracta.herencia.Coche;
+import com.appstracta.herencia.Moto;
+import com.appstracta.herencia.Vehiculo;
 
 public class AppHerencia {
 
@@ -16,6 +19,27 @@ public class AppHerencia {
 
 		System.out.println("Ruedas del coche :: " + coche.getRuedas());
 		System.out.println("Marca del coche :: " + coche.getMarca());
+
+		// Muestra toString
+		System.out.println(coche.toString());
+
+		// Creamos instancia de moto
+		Moto moto = new Moto();
+
+		System.out.println("Moto :: " + moto.doblar());
+		System.out.println("Coche :: " + coche.doblar());
+
+		// Creamos objeto de Bus
+		Bus bus = new Bus();
+		bus.setEjes(3);
+		System.out.println("número ejes del bus :: " + bus.getEjes());
+
+		// Polimorfismo
+		Vehiculo bus2 = new Bus();
+		bus2.setColor("Blanco");
+
+		System.out.println("Color del bus 2 :: " + bus2.getColor());
+
 	}
 
 }
